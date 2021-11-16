0ae316c96b5d3bc7fb1d985185c238f54b031df9
Ercole19 <71319051+Ercole19@users.noreply.github.com>
2021-11-16 18:39:26 +0100

    Atena-ercole

---
+++ Progetti-Intellij/Ercole-atena/src/main/java/com/example/atena/controllerAggiungiEsame.java
@@ -0,0 +1,64 @@
+package com.example.atena;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+
+public class controllerAggiungiEsame {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void initAggiungiEsame () throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Aggiungi_Esame_view.fxml"));
+        stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setResizable(false);
+        scene = new Scene(root);
+        stage.setTitle("Aggiungi esame");
+        stage.setScene(scene);
+        stage.showAndWait();
+
+    }
+    public void initMostraMedia () throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Mostra_Media_View.fxml"));
+        stage = new Stage();
+        stage.setResizable(false);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("La tua media");
+        stage.showAndWait();
+
+    }
+
+    public void confermaEsame (ActionEvent event) throws IOException {
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        TextField NomeEsame = (TextField) stage.getScene().lookup("#Nome_Esame");
+        if (NomeEsame.getText().equals("")) {
+            System.out.println("Aggiungi un nome prima!");
+        }
+        else {
+            stage.close();
+        }
+    }
+
+    public void Indietro(ActionEvent event){
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+
+
+
+}
