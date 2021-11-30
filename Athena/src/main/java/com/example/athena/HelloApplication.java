ad146be92d87fd63de8bdbac75e894a3853d207a
Ranakit <luca.molinaro@students.uniroma2.eu>
2021-11-30 21:26:18 +0100

    Application Icon Added

--- Athena/src/main/java/com/example/athena/HelloApplication.java
+++ Athena/src/main/java/com/example/athena/HelloApplication.java
@@ -3,16 +3,21 @@ package com.example.athena;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("Hello!");
+        stage.setTitle("Athena");
+        Image icon = new Image(new File("src/main/resources/assets/icon.png").toURI().toString());
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
     }
