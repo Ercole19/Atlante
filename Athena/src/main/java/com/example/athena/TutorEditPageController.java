90e02e21eb2e7de29eb1c645ed6dbfdd651d0e99
Luca Molinaro <luca.molinaro@students.uniroma2.eu>
2021-12-01 15:14:30 +0100

    added Tutor edit page controller

--- Athena/src/main/java/com/example/athena/TutorEditPageController.java
+++ Athena/src/main/java/com/example/athena/TutorEditPageController.java
@@ -4,6 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -29,5 +32,20 @@ public class TutorEditPageController {
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
+    }
+
+    public void onCVButtonClick(ActionEvent event) throws IOException
+    {
+        AnchorPane root = new AnchorPane() ;
+        root.setPrefSize(600, 800) ;
+        Label CV = new Label("Lorem Ipsum Dolor Sit Amet") ;
+        root.getChildren().add(CV) ;
+        Scene CVScene = new Scene(root) ;
+        Stage tempStage = new Stage() ;
+        tempStage.setScene(CVScene) ;
+        tempStage.initModality(Modality.APPLICATION_MODAL) ;
+        tempStage.setResizable(false) ;
+        tempStage.setTitle("CV") ;
+        tempStage.showAndWait() ;
     }
 }
