1762cc09e031808b15c838dcd30829766a5ebedb
Ercole19 <71319051+Ercole19@users.noreply.github.com>
2021-12-20 19:49:20 +0100

    Introdotto login molto semplice

--- Athena/src/main/java/com/example/athena/LoginPageController.java
+++ Athena/src/main/java/com/example/athena/LoginPageController.java
@@ -1,13 +1,16 @@
 package com.example.athena;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Objects;
 
 import static javafx.fxml.FXMLLoader.load;
@@ -17,13 +20,29 @@ public class LoginPageController {
     private Parent root;
     private Stage stage;
     private Scene scene;
+    private studentDAO stDAO ;
+    @FXML
+    private TextField emailField ;
+    @FXML
+    private TextField passField;
 
     public void switchToMainPage(ActionEvent event) throws IOException
+            //email = emailprova
+            // pass = passprova
     {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainPageStudents.fxml"))) ;
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow() ;
-        scene = new Scene(root) ;
-        stage.setScene(scene) ;
+        String email = emailField.getText() ;
+        String password = passField.getText() ;
+        stDAO = new studentDAO() ;
+        if (stDAO.findStudent(email , password)) {
+            System.out.println("Access granted !");
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainPageStudents.fxml")));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+        }
+        else {
+            System.out.println("Access denied !");
+        }
     }
 
     public void switchtoTutorMainPage(ActionEvent event) throws IOException
--- Athena/src/main/java/module-info.java
+++ Athena/src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.example.athena {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires java.sql;
 
     opens com.example.athena to javafx.fxml;
     exports com.example.athena;
