605161149c8b69b052351d481ae615a9e0a9dc2b
sebastianopriscan <sebastianroberto.opriscan@gmail.com>
2022-02-03 20:23:50 +0100

    Added event types to Classes touched by the commit, not tested, building of the scene should be reviewed to show the type of event already added

--- Athena/src/main/java/com/example/athena/entities/EventDao.java
+++ Athena/src/main/java/com/example/athena/entities/EventDao.java
@@ -7,11 +7,11 @@ import java.time.LocalTime;
 public class EventDao extends AbstractDAO {
 
     private String email = User.getUser().getEmail();
-    private String addQuery = "INSERT INTO athena.eventi (`dataEvento`, `eventName`, `eventStart`, `eventEnd`, `eventDesc`, `utente`) values (?,?,?,?,?,?)" ;
-    private String getEventInfo = "select eventName , eventStart , eventEnd , eventDesc , dataEvento from athena.eventi where dataEvento = ? and utente = ? " ;
+    private String addQuery = "INSERT INTO athena.eventi (`dataEvento`, `eventName`, `eventStart`, `eventEnd`, `eventDesc`, `utente`, `type`) values (?,?,?,?,?,?,?)" ;
+    private String getEventInfo = "select eventName , eventStart , eventEnd , eventDesc , dataEvento, type from athena.eventi where dataEvento = ? and utente = ? " ;
 
 
-    public void addEvent(LocalDate data , String name , LocalTime start ,LocalTime end , String description ) {
+    public void addEvent(LocalDate data , String name , LocalTime start ,LocalTime end , String description , String type) {
 
         try (PreparedStatement statement = this.getConnection().prepareStatement(addQuery)) {
 
@@ -21,6 +21,7 @@ public class EventDao extends AbstractDAO {
             statement.setTime(4, Time.valueOf(end));
             statement.setString(5, description);
             statement.setString(6 , email);
+            statement.setString(7, type);
 
             statement.executeUpdate() ;
 
@@ -45,7 +46,8 @@ public class EventDao extends AbstractDAO {
                 eventinfos[i + 2] = String.valueOf(set.getTime(3));
                 eventinfos[i + 3] = set.getString(4) ;
                 eventinfos[i+4] = String.valueOf(set.getDate(5));
-                i = i + 5 ;
+                eventinfos[i+5] = set.getString(6) ;
+                i = i + 6 ;
 
 
             }
@@ -73,16 +75,17 @@ public class EventDao extends AbstractDAO {
         }
     }
 
-    public void updateEvento(LocalDate data , String name , LocalTime start ,LocalTime end , String description , String oldname) {
-        try (PreparedStatement statement = this.getConnection().prepareStatement("update athena.eventi set dataEvento = ? , eventName = ? , eventStart = ? , eventEnd = ? , eventDesc = ? where utente = ? and dataEvento = ? and eventName = ?")){
+    public void updateEvento(LocalDate data , String name , LocalTime start ,LocalTime end , String description , String oldname, String type) {
+        try (PreparedStatement statement = this.getConnection().prepareStatement("update athena.eventi set dataEvento = ? , eventName = ? , eventStart = ? , eventEnd = ? , eventDesc = ? , type = ? where utente = ? and dataEvento = ? and eventName = ?")){
             statement.setDate(1, Date.valueOf(data));
             statement.setString(2,name);
             statement.setTime(3, Time.valueOf(start));
             statement.setTime(4, Time.valueOf(end));
             statement.setString(5,description);
-            statement.setString(6,email);
-            statement.setDate(7, Date.valueOf(data));
-            statement.setString(8,oldname);
+            statement.setString(6, type) ;
+            statement.setString(7,email) ;
+            statement.setDate(8, Date.valueOf(data)) ;
+            statement.setString(9,oldname) ;
 
             statement.execute();
         }catch (SQLException exc ){
