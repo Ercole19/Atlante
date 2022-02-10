d67ba8a8d30d6bac3942d951c90c6203be061df6
sebastianopriscan <sebastianroberto.opriscan@gmail.com>
2022-02-10 19:33:33 +0100

    Now the ShowCVGraphicalController, upon initialization, will add a Listener to the loader that will disable the href in HTML files.

--- Athena/src/main/java/com/example/athena/graphical_controller/ShowCVGraphicalController.java
+++ Athena/src/main/java/com/example/athena/graphical_controller/ShowCVGraphicalController.java
@@ -1,9 +1,14 @@
 package com.example.athena.graphical_controller;
 
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -17,7 +22,21 @@ public class ShowCVGraphicalController implements Initializable, PostInitialize
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
-
+        cvWebView.getEngine().getLoadWorker().stateProperty().addListener((observableValue, state, t1) -> {
+            if(t1 == Worker.State.SUCCEEDED)
+            {
+                Document doc = cvWebView.getEngine().getDocument() ;
+                NodeList list = doc.getElementsByTagName("a") ;
+               for(int i = 0; i < list.getLength(); i++)
+                {
+                    Node element = list.item(i) ;
+                    NamedNodeMap map = element.getAttributes() ;
+                    Node href = map.getNamedItem("href") ;
+                    if(href != null) href.setNodeValue("") ;
+                }
+            }
+        });
+        cvWebView.setContextMenuEnabled(false) ;
     }
 
     @Override
