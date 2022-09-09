1022e8cbbcadead7790a6f264f24d784ed489e46
Ercole19 <71319051+Ercole19@users.noreply.github.com>
2022-09-09 18:57:45 +0200

    Finished oracle interface

---
+++ Athena/src/main/java/com/example/athena/beans/oracle/OracleTutorCvInfoBean.java
@@ -0,0 +1,20 @@
+package com.example.athena.beans.oracle;
+
+import com.example.athena.beans.TutorCvInfoBean;
+
+import java.io.File;
+
+public class OracleTutorCvInfoBean implements TutorCvInfoBean {
+    
+    private File file ;
+    
+    @Override
+    public File getCV() {
+        return this.file ;
+    }
+    
+    public void setFile(String pathName) {
+        this.file = new File(pathName) ;
+        
+    }
+}
