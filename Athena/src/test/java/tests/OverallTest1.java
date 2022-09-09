c7362ecb8f2acc47b55f90a3d2e225726a6f8be1
seb <sebastianroberto.opriscan@gmail.com>
2022-09-09 19:28:38 +0200

    OverallTest pushing

--- Athena/src/test/java/tests/OverallTest1.java
+++ Athena/src/test/java/tests/OverallTest1.java
@@ -1,18 +1,20 @@
 package tests;
 
-import com.example.athena.beans.normal.SearchTutorQueryBean;
-import com.example.athena.beans.normal.TutorSearchResultBean;
-import com.example.athena.beans.normal.UserBean;
+import com.example.athena.beans.ReviewTutorSendUsernameBean;
+import com.example.athena.beans.normal.*;
 import com.example.athena.entities.ByCourseOrNameEnum;
-import com.example.athena.exceptions.FindException;
-import com.example.athena.exceptions.FindTutorException;
-import com.example.athena.exceptions.UserInfoException;
-import com.example.athena.exceptions.UserNotFoundException;
+import com.example.athena.entities.ReviewEntity;
+import com.example.athena.entities.TutorReviewCodesGenerator;
+import com.example.athena.entities.User;
+import com.example.athena.exceptions.*;
 
 import com.example.athena.use_case_controllers.LoginUseCaseController;
+import com.example.athena.use_case_controllers.ReviewTutorUseCaseController;
 import com.example.athena.use_case_controllers.SearchTutorUseCaseController;
 import org.junit.jupiter.api.Test;
 
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,27 +25,76 @@ import static org.junit.jupiter.api.Assertions.fail ;
 
 public class OverallTest1 {
 
+
     @Test
     public void test1() {
 
+        login("alba@student.it", "tramonto");
+
+        boolean retVal = searchBy(ByCourseOrNameEnum.BY_COURSE, "Fondamenti") && searchBy(ByCourseOrNameEnum.BY_NAME, "Kanye") ;
+        assertTrue(retVal) ;
+
+    }
+
+    @Test
+    public void test2() {
+        login("tutor@tutor.it", "rotut") ;
+
+        ReviewTutorSendUsernameBean bean = new NormalReviewTutorSendUsernameBean(
+                "alba@student.it",
+                "Fondamenti",
+                LocalDate.of(2021,12, 8),
+                13, 13,13,14
+        ) ;
+
+        String code = pushCodeToDB(bean) ;
+
+        User.logout();
+
+        login("alba@student.it", "tramonto") ;
+
+        SendReviewBean review = new SendReviewBean(5, code) ;
+
+        try {
+            new ReviewTutorUseCaseController().sendReview(review) ;
+        } catch (TutorReviewException e) {
+            fail() ;
+        }
+
+        assertTrue(true);
+
+    }
+
+    private String pushCodeToDB(ReviewTutorSendUsernameBean usernameBean) {
+        String reviewCode = null;
+        try {
+            reviewCode = TutorReviewCodesGenerator.generateReviewCode(5);
+            ReviewEntity review = new ReviewEntity(usernameBean, reviewCode) ;
+            review.toDB() ;
+
+        } catch (NoSuchAlgorithmException | TutorReviewException e) {
+            fail() ;
+        }
+
+        return reviewCode ;
+    }
+
+    private void login(String username, String password) {
         UserBean params = new UserBean() ;
-        params.setEmail("alba@student.it");
-        params.setPassword("tramonto") ;
+        params.setEmail(username);
+        params.setPassword(password) ;
         LoginUseCaseController controller = new LoginUseCaseController() ;
         try {
             controller.findUser(params) ;
         } catch (UserNotFoundException | UserInfoException | FindException e) {
             fail() ;
         }
-        boolean retVal = searchBy(ByCourseOrNameEnum.BY_COURSE, "Fondamenti") && searchBy(ByCourseOrNameEnum.BY_NAME, "Kanye") ;
-        assertTrue(retVal) ;
-
     }
 
     private boolean searchBy(ByCourseOrNameEnum searchEnum, String query) {
         SearchTutorQueryBean bean = new SearchTutorQueryBean() ;
-        if(searchEnum == ByCourseOrNameEnum.BY_COURSE) bean.setByCourseOrName("byCourse") ;
-        else bean.setByCourseOrName("byName") ;
+        if(searchEnum == ByCourseOrNameEnum.BY_COURSE) bean.setByCourseOrName("BY_COURSE") ;
+        else bean.setByCourseOrName("BY_NAME") ;
         bean.setByBestReviews(false) ;
         bean.setQuery(query);
 
@@ -57,9 +108,13 @@ public class OverallTest1 {
         boolean firstResult = true ;
 
         for(TutorSearchResultBean searchResultBean : beans) {
-            firstResult = firstResult && (searchResultBean.getName().contains(query)) ;
+            if (searchEnum == ByCourseOrNameEnum.BY_COURSE) firstResult = firstResult && (searchResultBean.getTaughtSubject().contains(query)) ;
+            else firstResult = firstResult && (searchResultBean.getName().contains(query)) ;
+
         }
 
         return firstResult ;
     }
+
+
 }
